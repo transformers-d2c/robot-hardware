8e8ee285de7fa13c89f1ccf37b31650cffdd8934
AlienX <64488123+AlienX2001@users.noreply.github.com>
2021-08-08 14:26:19 +0530

    basic skeletal code of the robot

---
+++ main.py
@@ -0,0 +1,52 @@
+import socket
+from machine import Pin,PWM
+
+def flip(a):
+    a.duty(100)
+    a.duty(40)
+
+def forward(a,b,c,d):
+    a.on()
+    b.off()
+    c.on()
+    d.off()
+
+def reverse(a,b,c,d):
+    a.off()
+    b.on()
+    c.off()
+    d.on()
+
+def left(a,b,c,d):
+    a.off()
+    b.on()
+    c.on()
+    d.off()
+
+def right(a,b,c,d):
+    a.on()
+    b.off()
+    c.off()
+    d.on()
+
+def main():
+    s =  socket.socket()
+    motor1pin1 =  Pin(A,Pin.OUT)
+    motor1pin2 = Pin(B, Pin.OUT)
+    motor2pin1 = Pin(C, Pin.OUT)
+    motor2pin2 = Pin(D, Pin.OUT)
+    servopin = Pin(E, Pin.OUT)
+    servo = PWM(servopin,freq = 50)
+    s.connect("IP")
+    while True:
+        data = s.recv(1024) # subject to change depending on the amount of data we get from socket
+        if data == "LEFT": # subject to change depending on the input we get from the socket
+            left(motor1pin1,motor1pin2,motor2pin1,motor2pin2)
+        if data == "RIGHT": # subject to change depending on the input we get from the socket
+            right(motor1pin1, motor1pin2, motor2pin1, motor2pin2)
+        if data == "FORWARD": # subject to change depending on the input we get from the socket
+            forward(motor1pin1, motor1pin2, motor2pin1, motor2pin2)
+        if data == "REVERSE": # subject to change depending on the input we get from the socket
+            reverse(motor1pin1, motor1pin2, motor2pin1, motor2pin2)
+        if data == "FLIP":
+            flip(servo)
