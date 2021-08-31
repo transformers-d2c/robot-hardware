8076965b9b5b6fcfa366466d852948746b71ef23
Dev Rajput <dev19034@iiitd.ac.in>
2021-08-31 15:57:33 +0530

    Add pin_config_util

---
+++ pin_config_util.py
@@ -0,0 +1,30 @@
+import json
+
+def get_pins():
+    with open('pinConfig.json','r') as inp:
+        data = json.load(inp)
+        print(str(data))
+
+
+def set_pins(lp,ln,rp,rn,s):
+    with open('pinConfig.json','w') as out:
+        data = {
+            'leftwheel':{
+                'pos':lp,
+                'neg':ln
+            },
+            'rightwheel':{
+                'pos':rp,
+                'neg':rn
+            },
+            'servo':s
+        }
+
+
+def set_pins_input():
+    lp = int(input("Left wheel positive:"))
+    ln = int(input("Left wheel negative:"))
+    rp = int(input("Right wheel positive:"))
+    rn = int(input("Right wheel negative:"))
+    s = int(input("Servo:"))
+    set_pins(lp,ln,rp,rn,s)
